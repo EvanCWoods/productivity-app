de9552fcd51e8cd5db7cac57890820bc49288e0e
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-02-21 07:33:02 +1100

    created the starter index.html file boilerr plate markdown, set up the express server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static('/Develop/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+})
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`App running on http://localhost:${PORT}`);
+})
